1bf26981eefbafa873ad545fba81881f7cb8d25a
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-27 18:40:31 +0530

    added loding-navbar loder side bar allworking

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -65,38 +65,33 @@ export const Sidebar = ({
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <div className="relative h-8 w-8 mr-4">
+    <div className="h-full bg-gradient-to-b from-[#192339] to-[#111827] text-white">
+      <div className="py-4 px-5 space-y-6">
+        <Link href="/dashboard" className="flex items-center space-x-2">
+          <div className="relative h-10 w-10">
             <Image fill alt="Logo" src="/logo.png" />
           </div>
-          <h1 className={cn("text-2xl font-bold", poppins.className)}>
-            Genius
-          </h1>
+          <h1 className="text-2xl font-extrabold">TextToAnything</h1>
         </Link>
-        <div className="space-y-1">
+        <div className="space-y-4">
           {routes.map((route) => (
             <Link
-              key={route.href} 
+              key={route.href}
               href={route.href}
               className={cn(
-                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
+                "flex items-center p-3 rounded-md transition duration-300 ease-in-out hover:bg-sky-400 hover:bg-opacity-25",
+                pathname === route.href ? "bg-sky-400 bg-opacity-25" : "",
               )}
             >
-              <div className="flex items-center flex-1">
-                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
-                {route.label}
+              <div className="flex items-center space-x-2">
+                <route.icon className="h-4 w-4" />
+                <span className="text-sm font-medium">{route.label}</span>
               </div>
             </Link>
           ))}
         </div>
       </div>
-      <FreeCounter 
-        apiLimitCount={apiLimitCount} 
-        isPro={isPro}
-      />
+      <FreeCounter apiLimitCount={apiLimitCount} isPro={isPro} />
     </div>
   );
 };
