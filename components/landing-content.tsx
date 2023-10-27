526118cd0d1dc191e14adcebfb7ef44af3d35d53
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-27 18:37:01 +0530

    added image loding-content and loding-hero

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Joel",
+    name: "Chat",
     avatar: "J",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
@@ -33,23 +31,26 @@ export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
       <h2 className="text-center text-4xl text-white font-extrabold mb-10">Testimonials</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
         {testimonials.map((item) => (
-          <Card key={item.description} className="bg-[#192339] border-none text-white">
+          <Card key={item.description} className="bg-[#192339] border-none text-white mx-auto p-6 rounded-lg shadow-lg">
             <CardHeader>
-              <CardTitle className="flex items-center gap-x-2">
+              <div className="flex items-center gap-x-2">
+                <div className="bg-sky-400 w-12 h-12 flex items-center justify-center text-white text-2xl font-bold rounded-full">
+                  {item.avatar}
+                </div>
                 <div>
-                  <p className="text-lg">{item.name}</p>
+                  <p className="text-lg font-bold">{item.name}</p>
                   <p className="text-zinc-400 text-sm">{item.title}</p>
                 </div>
-              </CardTitle>
-              <CardContent className="pt-4 px-0">
-                {item.description}
-              </CardContent>
+              </div>
             </CardHeader>
+            <CardContent className="pt-4 px-0 text-sm">
+              {item.description}
+            </CardContent>
           </Card>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
